bd266e3e2bfc19f6d03aa6bd9ffdf9c24a0f3eff
jgryn <jonathangryn@Jonathans-MacBook-Pro.local>
2019-11-23 23:58:25 -0800

    added billing and client side app!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 var mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -11,6 +12,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// Anytime GET, POST, PUT, PATCH, or DELETE or anything that has a request body and comes into our app, this middleware will parse the body and assign it to the req.body prop of the incoming req object
+app.use(bodyParser.json());
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -22,6 +25,19 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	// Express will seve up produciton assets
+	// like our main.js file, or main.css file!
+	app.use(express.static('client/build'));
+
+	// Express will serve up the index.html
+	// if it doesn't recognize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = app.listen(process.env.PORT || 5000);
 app.listen(PORT);
